ea0f3bce1f02a6d58566f7578858e9b2351328b2
yurrikon <yurrikon@gmail.com>
2022-07-25 00:14:30 +0300

    task completed both ways

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine();
-
-
-string[] array = {"cat", "dog", "zebra", "elefant", "fox", ":)", "A"};
-Console.WriteLine("Из следующих элементов массива:");
-for (int i = 0; i < array.Length; i = i + 1)
+﻿internal class Program
 {
-    Console.Write(array[i] + "\t");
-}
-Console.WriteLine();
-Console.WriteLine("Из трех и менее символов состоят:");
-for (int i = 0; i < array.Length; i = i + 1)
-{
-    if (array[i].Length < 4)
-        Console.Write(array[i] + "\t");
+    private static void Main(string[] args)
+    {
+        Console.WriteLine();
+
+
+        string[] array = { "cat", "dog", "zebra", "elefant", "fox", ":)", "A" };
+        Console.WriteLine("Из следующих элементов массива:");
+        for (int i = 0; i < array.Length; i = i + 1)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Из трех и менее символов состоят:");
+        for (int i = 0; i < array.Length; i = i + 1)
+        {
+            if (array[i].Length < 4)
+                Console.Write(array[i] + "\t");
+        }
+    }
 }
