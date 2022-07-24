e0f2c980e65a2ebf23811d1059844088d5ad78c4
yurrikon <yurrikon@gmail.com>
2022-07-24 22:15:48 +0300

    task completed filter

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine();
 
+
+string[] array = {"cat", "dog", "zebra", "elefant", "fox", ":)", "A"};
+Console.WriteLine("Из следующих элементов массива:");
+for (int i = 0; i < array.Length; i = i + 1)
+{
+    Console.Write(array[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine("Из трех и менее символов состоят:");
+for (int i = 0; i < array.Length; i = i + 1)
+{
+    if (array[i].Length < 4)
+        Console.Write(array[i] + "\t");
+}
