3362b82c5b78582eb8cab56240dcab52e8fba278
yurrikon <yurrikon@gmail.com>
2022-07-25 00:09:06 +0300

    schemes added

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine();
-
-string[] array = {"cat", "dog", "zebra", "elefant", "fox", ":)", "A"};
-Console.WriteLine("Из следующих элементов массива:");
-for (int i = 0; i < array.Length; i = i + 1)
+﻿
+internal class Program
 {
-    Console.Write(array[i] + "\t");
-}
-Console.WriteLine();
-Console.WriteLine("Из трех и менее символов состоят:");
-for (int i = 0; i < array.Length; i = i + 1)
-{
-    string[] array2 = new string[array.Length];
-    if (array[i].Length < 4)
+    private static void Main(string[] args)
     {
-        Array.Copy(array, array2, array.Length);
-        Console.Write(array2[i] + "\t");
-    }  
+        Console.WriteLine();
+        
+        string[] array = { "cat", "dog", "zebra", "elefant", "fox", ":)", "A" };
+        Console.WriteLine("Из следующих элементов массива:");
+        for (int i = 0; i < array.Length; i = i + 1)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        string[] array2 = new string[array.Length];
+        Console.WriteLine("Из трех и менее символов состоят:");
+        for (int i = 0; i < array.Length; i = i + 1)
+        {
+            
+            if (array[i].Length < 4)
+            {
+                Array.Copy(array, array2, array.Length);
+                Console.Write(array2[i] + "\t");
+            }
+        }
+    }
 }
